76bc52810f28b991f30c15d1f7393e8622e1eba2
Documenter.jl <documenter@juliadocs.github.io>
2024-04-05 16:00:29 +0000

    build based on 657ce14

---
+++ v0.0.17/assets/index.md.NlY88eeZ.lean.js
@@ -0,0 +1,15 @@
+import { _ as _export_sfc, c as createElementBlock, o as openBlock, a6 as createStaticVNode } from "./chunks/framework.r4j8LKyH.js";
+const __pageData = JSON.parse('{"title":"","description":"","frontmatter":{"layout":"home","hero":{"name":"DocumenterVitepress.jl","text":"Document your code","tagline":"A Markdown backend designed to work with VitePress and Documenter.jl","image":{"src":"/logo.png","alt":"DocumenterVitepress"},"actions":[{"theme":"brand","text":"Getting Started","link":"/getting_started"},{"theme":"alt","text":"View on Github","link":"https://github.com/LuxDL/DocumenterVitepress.jl"},{"theme":"alt","text":"API Examples","link":"/api"}]},"features":[{"title":"Markdown","details":"Write in standard markdown syntax","link":"/markdown-examples"},{"title":"Literate.jl","details":"Parse scripts into markdown via Literate.jl","link":"https://github.com/fredrikekre/Literate.jl"},{"title":"VUE components","details":"Explore the possibilities with VUE components","link":"https://vuejs.org/guide/essentials/component-basics"}]},"headers":[],"relativePath":"index.md","filePath":"index.md","lastUpdated":null}');
+const _sfc_main = { name: "index.md" };
+const _hoisted_1 = /* @__PURE__ */ createStaticVNode("", 2);
+const _hoisted_3 = [
+  _hoisted_1
+];
+function _sfc_render(_ctx, _cache, $props, $setup, $data, $options) {
+  return openBlock(), createElementBlock("div", null, _hoisted_3);
+}
+const index = /* @__PURE__ */ _export_sfc(_sfc_main, [["render", _sfc_render]]);
+export {
+  __pageData,
+  index as default
+};
